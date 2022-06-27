bbf1d90281a3f169aeb869951525dd264ad7243e
Rakesh kumar <dr.rakesh684@gmail.com>
2022-06-27 16:37:27 +0530

    Added Example.js for passing function as props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Menu from "./Menu"
 import Favorite from "./Favorite"
+import Example from "./Example"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
             <Menu something="whatever" />
             <hr />
             <Favorite />
+            <Example />
     </div>
   );
 }
